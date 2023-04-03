39c0624c8d4dbef0d77ede9b87530ae81ec4d10e
Supriseboy <justin.leach.work@gmail.com>
2023-04-03 16:25:02 -0400

    fix default route bug.

--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -14,16 +14,14 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+Route::redirect('/', '/login');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/', function () {
+    Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
