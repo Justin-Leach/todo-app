034a432b9ad49f5cfec0bcb3cac343f10eefaea6
Supriseboy <justin.leach.work@gmail.com>
2023-04-03 15:12:26 -0400

    completed css update password profile.

--- todo-app/resources/views/profile/update-password-form.blade.php
+++ todo-app/resources/views/profile/update-password-form.blade.php
@@ -1,4 +1,4 @@
-<x-jet-form-section submit="updatePassword">
+<x-form-section submit="updatePassword">
     <x-slot name="title">
         {{ __('Update Password') }}
     </x-slot>
@@ -7,22 +7,25 @@
         {{ __('Ensure your account is using a long, random password to stay secure.') }}
     </x-slot>
 
-    <x-slot name="form">
-        <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="current_password" value="{{ __('Current Password') }}" />
-            <x-jet-input id="current_password" type="password" class="mt-1 block w-full" wire:model.defer="state.current_password" autocomplete="current-password" />
+    <x-slot name="form" class="flex flex-col">
+        <div>
+            <label for="current_password" class="block font-medium text-sm text-gray-700" for="name">{{ __('Current Password') }}</label>
+            <input class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded shadow-sm"
+                id="current_password" type="password"wire:model.defer="state.current_password" autocomplete="current-password" >
             <x-jet-input-error for="current_password" class="mt-2" />
         </div>
 
-        <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="password" value="{{ __('New Password') }}" />
-            <x-jet-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password" />
+        <div class="mt-4">
+            <label for="password" class="block font-medium text-sm text-gray-700" for="name">{{ __('New Password') }}</label>
+            <input class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded shadow-sm"
+            id="password" type="password" wire:model.defer="state.password" autocomplete="new-password" >
             <x-jet-input-error for="password" class="mt-2" />
         </div>
 
-        <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-            <x-jet-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
+        <div class="mt-4">
+            <label for="password_confirmation" class="block font-medium text-sm text-gray-700" for="name">{{ __('Confirm Password') }}</label>
+            <input class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded shadow-sm"
+                id="password_confirmation" type="password" wire:model.defer="state.password_confirmation" autocomplete="new-password" >
             <x-jet-input-error for="password_confirmation" class="mt-2" />
         </div>
     </x-slot>
@@ -32,8 +35,13 @@
             {{ __('Saved.') }}
         </x-jet-action-message>
 
-        <x-jet-button>
-            {{ __('Save') }}
-        </x-jet-button>
+        <button class="relative w-32 inline-block text-lg group disabled:opacity-25" wire:loading.attr="disabled" wire:target="photo">
+            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+            <span class="absolute left-0 w-48 h-48 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+            <span class="relative">{{ __('Save') }}</span>
+            </span>
+            <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+        </button>
     </x-slot>
-</x-jet-form-section>
+</x-form-section>
