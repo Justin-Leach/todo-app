fb8921b75f2523799eedcfda9a5324f28738f805
Supriseboy <justin.leach.work@gmail.com>
2023-04-03 16:01:01 -0400

    completed browser sessions UI.

---
+++ todo-app/resources/views/components/action-section.blade.php
@@ -0,0 +1,12 @@
+<div {{ $attributes->merge(['class' => 'flex flex-col']) }}>
+    <x-jet-section-title>
+        <x-slot name="title">{{ $title }}</x-slot>
+        <x-slot name="description">{{ $description }}</x-slot>
+    </x-jet-section-title>
+
+    <div class="flex flex-col">
+        <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+            {{ $content }}
+        </div>
+    </div>
+</div>
