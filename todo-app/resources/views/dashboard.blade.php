ed69c748e4f37007e61c85f8495c2acd627fe845
Supriseboy <justin.leach.work@gmail.com>
2023-04-03 23:39:13 -0400

    improved UI dashbord and change navbar to a siderbar.

--- todo-app/resources/views/dashboard.blade.php
+++ todo-app/resources/views/dashboard.blade.php
@@ -1,15 +1,34 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="py-8">
+        <div class="mx-auto sm:px-6 lg:px-8">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+            <div class="flex flex-col justify-center items-center space-y-8">
+                <div class="flex flex-row space-x-16">
+                    <div class="bg-red-500 w-64 h-48 shadow-xl rounded">
+                        <div class="flex flex-col h-full justify-center items-center">
+                            <div class="text-8xl">
+                                {{ __('99') }}
+                            </div>
+                            <div class="mt-4 text-xl">
+                                {{ __('Upcoming Task') }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-blue-500 w-64 h-48 shadow-xl rounded">
+                        <div class="flex flex-col h-full justify-center items-center">
+                            <div class="text-8xl">
+                                {{ __('05') }}
+                            </div>
+                            <div class="mt-4 text-xl">
+                                {{ __('Task Completed') }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-green-500 h-screen w-full">
+                </div>
             </div>
+
         </div>
     </div>
 </x-app-layout>
